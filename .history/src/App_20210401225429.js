d8d0ec1d0ec2337839f922c279ca5a666104ad26
nikitaoak <nikita.oak28@gmail.com>
2021-04-01 22:55:17 +0200

    added react

---
+++ .history/src/App_20210401225429.js
@@ -0,0 +1,16 @@
+import logo from './Image/nikita.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello, my name is Nikita. I am a front-end Developer.</h1>
+          <h2>Welcome to my page</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
